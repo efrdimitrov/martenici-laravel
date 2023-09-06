f8a163fde4d4e15690448ca76cfb6847e2747277
efrdimitrov <efrdimitrov@abv.bg>
2023-09-06 15:25:10 +0300

    change color

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <meta name="theme-color" content="#EF4444"> <!--   color chrome mobile -->
+    <meta name="theme-color" content="#a15757"> <!--   color chrome mobile -->
 
     <!-- lightgallery -->
     <link type="text/css" rel="stylesheet" href="lightgallery/css/lightgallery.css" />
